7ba493b0683f895e20caaaa793f707eedf76847d
Mohamed Khair <83073489+mohamed-arabi16@users.noreply.github.com>
2025-08-01 12:58:49 +0300

    refactor: move code out of app folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
-  ],
   darkMode: 'class',
+  content: [
+    'app/**/*.{tsx,ts}',
+    'src/**/*.{tsx,ts}'
+  ],
   theme: {
     extend: {},
   },
